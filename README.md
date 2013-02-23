c5110babf39fa4ef8f0c9fc9df51f0d0678bbc1c
Simon Goellner <simey.me@gmail.com>
2013-02-23 17:23:18 +0000

    added minified version of script.
    general tidying up.

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 jQuery-ui-Slider-Pips
 =====================
-
-Plugin for adding little 'pips' to a slider.
-----------------------------------------------------------
-
-
+  
+Plugin for adding little 'pips' to a slider.  
+----------------------------------------------------------  
+  
+  
 This plugin 'extends' the jQuery UI Slider widget.    
 check out the Demo at: http://sites.simey.me/jquery-pip/    
-
-
+  
+  
+------------------------------------  
+  
+  
+Usage:   
+Include the plugin javascript file after jquery-ui.   
+Include the CSS file, edit as you please.  
+  
+  
 ------------------------------------
+
+Tested:   
+Chrome, Firefox, IE8+  
+  
+   
+Would need some work for IE7 styling issues (`display: inline-block;`)
 
 
 
--- src/js/jquery-ui-slider-pips.js
+++ src/js/jquery-ui-slider-pips.js
@@ -1,80 +1,72 @@
+/* jquery slider pips plugin, version 0.1 */
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-		(function($) {
+	(function($) {
+		
+		var extensionMethods = {
 			
-			var extensionMethods = {
+			pips: function( settings ) {
 				
-				pips: function( settings ) {
+				options = {
 					
-					options = {
+					first: 	"number", 	// "pip" , false
+					last: 	"number", 	// "pip" , false
+					rest: 	"pip" 		// "number" , false
+					
+				};
+				
+				$.extend( options, settings );
+				
+				// get rid of all pips that might already exist.
+				this.element.addClass('ui-slider-pips').find( '.ui-slider-pip' ).remove();
+				
+				// we need teh amount of pips to create.
+				var pips = this.options.max - this.options.min;					
+				 
+					// for every stop in the slider(-1), we create a pip.
+					for( i=0; i<=pips; i++ ) {
 						
-						first: 	"number", 	// "pip" , false
-						last: 	"number", 	// "pip" , false
-						rest: 	"pip" 		// "number" , false
+						// hold a span element for the pip
+						var s = $('<span class="ui-slider-pip"><span class="ui-slider-line"></span><span class="ui-slider-number">'+i+'</span></span>');
 						
-					};
-					
-					$.extend( options, settings );
-					
-					// get rid of all pips that might already exist.
-					this.element.addClass('ui-slider-pips').find( '.ui-slider-pip' ).remove();
-					
-					// we need teh amount of pips to create.
-					var pips = this.options.max - this.options.min;					
-					 
-					 	// for every stop in the slider(-1), we create a pip.
-						for( i=0; i<=pips; i++ ) {
-							
-							// hold a span element for the pip
-							var s = $('<span class="ui-slider-pip"><span class="ui-slider-line"></span><span class="ui-slider-number">'+i+'</span></span>');
-							
-							// add a class so css can handle the display
-							// we'll hide numbers by default in CSS, and show them if set.
-							// we'll also use CSS to hide the pip altogether.
-							if( 0 == i ) {
-								s.addClass('ui-slider-pip-first');
-								if( "number" == options.first ) { s.addClass('ui-slider-pip-number'); }
-								if( false == options.first ) { s.addClass('ui-slider-pip-hide'); }
-							} else if ( pips == i ) {
-								s.addClass('ui-slider-pip-last');
-								if( "number" == options.last ) { s.addClass('ui-slider-pip-number'); }
-								if( false == options.last ) { s.addClass('ui-slider-pip-hide'); }
-							} else {
-								if( "number" == options.rest ) { s.addClass('ui-slider-pip-number'); }
-								if( false == options.rest ) { s.addClass('ui-slider-pip-hide'); }
-							}
-							
-							
-							// if it's a horizontal slider we'll set the left offset,
-							// and the top if it's vertical.
-							if( this.options.orientation == "horizontal" ) 
-								s.css({ left: '' + (100/pips)*i + '%'  });
-							else
-								s.css({ top: '' + (100/pips)*i + '%'  });
-							
-							
-							// append the span to the slider.
-							this.element.append( s );
-						
+						// add a class so css can handle the display
+						// we'll hide numbers by default in CSS, and show them if set.
+						// we'll also use CSS to hide the pip altogether.
+						if( 0 == i ) {
+							s.addClass('ui-slider-pip-first');
+							if( "number" == options.first ) { s.addClass('ui-slider-pip-number'); }
+							if( false == options.first ) { s.addClass('ui-slider-pip-hide'); }
+						} else if ( pips == i ) {
+							s.addClass('ui-slider-pip-last');
+							if( "number" == options.last ) { s.addClass('ui-slider-pip-number'); }
+							if( false == options.last ) { s.addClass('ui-slider-pip-hide'); }
+						} else {
+							if( "number" == options.rest ) { s.addClass('ui-slider-pip-number'); }
+							if( false == options.rest ) { s.addClass('ui-slider-pip-hide'); }
 						}
+						
+						
+						// if it's a horizontal slider we'll set the left offset,
+						// and the top if it's vertical.
+						if( this.options.orientation == "horizontal" ) 
+							s.css({ left: '' + (100/pips)*i + '%'  });
+						else
+							s.css({ top: '' + (100/pips)*i + '%'  });
+						
+						
+						// append the span to the slider.
+						this.element.append( s );
 					
-				}
+					}
 				
-				
-			};
-		
-			$.extend(true, $['ui']['slider'].prototype, extensionMethods);
-		
-		
-		})(jQuery);
-		
+			}
+			
+			
+		};
+	
+		$.extend(true, $['ui']['slider'].prototype, extensionMethods);
+	
+	
+	})(jQuery);
+	
 		
 		
