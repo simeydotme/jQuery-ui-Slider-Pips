b71a773a948cc15a3169f19333348bd3028f4e5b
Simon Goellner <simey.me@gmail.com>
2013-02-23 16:09:48 +0000

    first commit of the demo files

---
+++ demo/js/main.js
@@ -0,0 +1,41 @@
+
+
+
+
+$(document).ready( function() {
+
+
+		prettyPrint();
+
+		$('.slider1').slider({ max: 10, min: 0, value: 0 }).slider('pips', { rest: 'number' });
+
+		$('.slider2').slider({ max: 200, min: 0, value: 0 }).slider('pips');
+
+		$('.slider3').slider({ max: 45, min: 0, range: true, values: [10, 35] }).slider('pips', { rest: false });
+
+		$('.slider4').slider({ max: 10, min: 0, value: 10, range: "min", orientation: 'vertical' }).slider('pips', { rest: 'number' });
+
+		$('.slider5').slider({ max: 45, min: 0, range: true, values: [10, 35], orientation: 'vertical' }).slider('pips', { rest: false });
+
+		$('.slider6').slider({ max: 17, min: 0, orientation: 'vertical' }).slider('pips', { first: 'pip', last: 'pip' });
+
+
+
+		$('pre').hide();
+		$('h1').after( function() {
+			
+			var $a = $('<button>show code examples</button>');
+			$a.on('click', function() {
+				$('pre, br').fadeToggle();	
+			});
+			
+			return $a;
+			
+		});
+
+});
+
+
+
+
+
