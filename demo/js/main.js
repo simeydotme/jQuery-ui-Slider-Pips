b0d7fb4495aece5e6ea721f9f48040525d1fe2f1
Simon Goellner <simey.me@gmail.com>
2013-03-02 00:55:37 +0000

    Improve the demo greatly.
    change jQuery UI Theme
    add bootstrap

--- demo/js/main.js
+++ demo/js/main.js
@@ -7,31 +7,18 @@ $(document).ready( function() {
 
 		prettyPrint();
 
-		$('.slider1').slider({ max: 10, min: 0, value: 0 }).slider('pips', { rest: 'number' });
+		$('.slider1').slider({ max: 100, min: 0, value: 0 }).slider('pips');
 
-		$('.slider2').slider({ max: 200, min: 0, value: 0 }).slider('pips');
+		$('.slider2').slider({ max: 10, min: 0, value: 0 }).slider('pips', { rest: 'number' });
 
 		$('.slider3').slider({ max: 45, min: 0, range: true, values: [10, 35] }).slider('pips', { rest: false });
 
-		$('.slider4').slider({ max: 10, min: 0, value: 10, range: "min", orientation: 'vertical' }).slider('pips', { rest: 'number' });
+		$('.slider4').slider({ max: 20, min: 0, value: 10, range: "min" }).slider('pips', { first: 'pip', last: 'pip' });
 
-		$('.slider5').slider({ max: 45, min: 0, range: true, values: [10, 35], orientation: 'vertical' }).slider('pips', { rest: false });
+		$('.slider5').slider({ max: 10, min: 0, range: true, values: [2, 8], orientation: 'vertical' }).slider('pips', { rest: false });
 
-		$('.slider6').slider({ max: 17, min: 0, orientation: 'vertical' }).slider('pips', { first: 'pip', last: 'pip' });
+		$('.slider6').slider({ max: 10, min: 0, orientation: 'vertical' }).slider('pips', { first: 'pip', last: 'pip' });
 
-
-
-		$('pre').hide();
-		$('hr').before( function() {
-			
-			var $a = $('<button>show code examples</button>');
-			$a.on('click', function() {
-				$('pre, br').fadeToggle();	
-			});
-			
-			return $a;
-			
-		});
 
 });
 
