5d2c1a465a5a3e2a6dc97af49556a2c8882beda9
Simon Goellner <simey.me@gmail.com>
2013-03-13 19:27:06 +0000

    update demo!

--- demo/js/main.js
+++ demo/js/main.js
@@ -7,21 +7,23 @@ $(document).ready( function() {
 
 		prettyPrint();
 
-		$('.slider1').slider({ max: 100, min: 0, value: 0 }).slider('pips');
+		$('.slider1')	.slider({ max: 10, min: 0, value: 3 }).slider('pips');
+		
+		$('.slider2')	.slider({ max: 6, min: -6, value: 0 }).slider('pips', { rest: 'label' });
+		
+		$('.slider21')	.slider({ max: 12, min: 1, value: 3 }).slider('pips', { rest: 'label', labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'] });
 
-		$('.slider2').slider({ max: 6, min: -6, value: 0 }).slider('pips', { rest: 'number' });
+		$('.slider3')	.slider({ max: 45, min: 0, range: true, values: [10, 35] }).slider('pips', { rest: false });
 
-		$('.slider3').slider({ max: 45, min: 0, range: true, values: [10, 35] }).slider('pips', { rest: false });
+		$('.slider4')	.slider({ max: 20, min: 0, value: 10, range: "min" }).slider('pips', { first: 'pip', last: 'pip' });
 
-		$('.slider4').slider({ max: 20, min: 0, value: 10, range: "min" }).slider('pips', { first: 'pip', last: 'pip' });
+		$('.slider5')	.slider({ max: 10, min: 0, range: true, values: [2, 8], orientation: 'vertical' }).slider('pips', { rest: false });
 
-		$('.slider5').slider({ max: 10, min: 0, range: true, values: [2, 8], orientation: 'vertical' }).slider('pips', { rest: false });
+		$('.slider6')	.slider({ max: 10, min: 0, orientation: 'vertical' }).slider('pips', { first: 'pip', last: 'pip' });
 
-		$('.slider6').slider({ max: 10, min: 0, orientation: 'vertical' }).slider('pips', { first: 'pip', last: 'pip' });
+		$('.slider7')	.slider({ max: 10, min: 0, range: true, values: [2, 8] }).slider('float');
 
-		$('.slider7').slider({ max: 10, min: 0, range: true, values: [2, 8] }).slider('float');
-
-		$('.slider8').slider({ max: 20, min: 0 }).slider('pips').slider('float');
+		$('.slider8')	.slider({ max: 20, min: 0 }).slider('pips').slider('float');
 
 
 });
