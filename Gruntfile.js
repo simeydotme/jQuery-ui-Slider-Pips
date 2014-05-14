0c231056840b1adf40a181842bd6d07c8010c4ff
simeydotme <simey.me@gmail.com>
2014-05-14 11:33:23 +0100

    updating to .jshintrc file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,89 +1,77 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON("package.json"),
 
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-   
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['src/js/jquery-ui-slider-pips.js'],
-        dest: 'dist/jquery-ui-slider-pips.js'
-      }
-    },
+        banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " + 
+                "<%= grunt.template.today(\"yyyy-mm-dd\") %>\n" + 
+                "<%= pkg.homepage ? \"* \" + pkg.homepage + \"\\n\" : \"\" %>" + 
+                "* Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= pkg.author.name %>;" + 
+                " Licensed <%= _.pluck(pkg.licenses, \"type\").join(\", \") %> */\n",
 
-
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/jquery-ui-slider-pips.min.js'
-      }
-    },
+        // Task configuration.
+        concat: {
+            options: {
+                banner: "<%= banner %>",
+                stripBanners: true
+            },
+            dist: {
+                src: ["src/js/jquery-ui-slider-pips.js"],
+                dest: "dist/jquery-ui-slider-pips.js"
+            }
+        },
 
 
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
+        uglify: {
+            options: {
+                banner: "<%= banner %>"
+            },
+            dist: {
+                src: "<%= concat.dist.dest %>",
+                dest: "dist/jquery-ui-slider-pips.min.js"
+            }
+        },
+
+
+        jshint: {
+            jshintrc: true,
+            gruntfile: {
+                src: "Gruntfile.js"
+            },
+            lib_test: {
+                src: ["src/js/*.js"]
+            }
+        },
+
+
+        copy: {
+            options: {
+                banner: "<%= banner %>"
+            },
+            main: {
+                files: [{
+                    src: ["src/css/*"],
+                    dest: "dist/",
+                    filter: "isFile",
+                    expand: true,
+                    flatten: true
+                }]
+            }
         }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['src/js/*.js']
-      }
-    },
 
 
-    copy: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      main: {
-        files: [
-          {src: ['src/css/*'], dest: 'dist/', filter: 'isFile', expand: true, flatten: true}        
-        ]
-      }
-    }
+    });
 
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
-  });
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'copy']);
+    // Default task.
+    grunt.registerTask("default", ["jshint", "concat", "uglify", "copy"]);
 
 };
