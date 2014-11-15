5e8cdc00f017d2de0ba8880ca20b08e50e37a2d2
simeydotme <simey.me@gmail.com>
2014-11-16 00:12:17 +0800

    create function to remove whitespace form pre text

---
+++ src/app/js/app.js
@@ -0,0 +1,10 @@
+
+(function($) {
+
+    $(function() {
+
+        $("pre").prettyPre().pretty();
+
+    });
+
+}(jQuery));
---
+++ src/app/js/prettyPre.js
@@ -0,0 +1,49 @@
+
+(function($) {
+
+    $(function() {
+
+        $.fn.prettyPre = function( obj ) {
+
+            var defaults = { type: "space" },
+                settings, str;
+
+            settings = $.merge( defaults, obj || {} );
+
+            str = ( settings.type === "space" ) ? " " : "\t";
+
+            return $(this).each( function() {
+
+                var $this = $(this),
+                    html = $this.html(),
+                    lead = "",
+                    remove;
+
+                while( html.indexOf( str ) === 0 ) {
+
+                    lead += str;
+                    html = html.substring(1);
+
+                }
+
+                remove = new RegExp("\n" + lead, "g");
+                html = html.replace(remove, "\n");
+
+                $this.html( html );
+
+            });
+
+        };
+
+        $.fn.pretty = function() {
+
+            $(this).addClass("prettyprint");
+            prettyPrint();
+
+            return $(this);
+
+        };
+
+    });
+
+}(jQuery));
