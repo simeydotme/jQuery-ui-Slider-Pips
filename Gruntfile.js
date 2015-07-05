bb00581f8950657e4497f44909e6d91ffb377f5a
simeydotme <simey.me@gmail.com>
2015-07-05 19:45:40 +0800

    fix jquery ui bower link

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,10 +137,10 @@ module.exports = function(grunt) {
                     "dist/js/jquery-plus-ui.min.js": [
 
                         "bower_components/jquery/dist/jquery.js",
-                        "bower_components/jqueryui/ui/core.js",
-                        "bower_components/jqueryui/ui/widget.js",
-                        "bower_components/jqueryui/ui/mouse.js",
-                        "bower_components/jqueryui/ui/slider.js"
+                        "bower_components/jquery-ui/ui/core.js",
+                        "bower_components/jquery-ui/ui/widget.js",
+                        "bower_components/jquery-ui/ui/mouse.js",
+                        "bower_components/jquery-ui/ui/slider.js"
 
                     ]
                 }
