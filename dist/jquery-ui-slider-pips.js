76dcc247845ffc842186575217f38f6a57a577df
simeydotme <simey.me@gmail.com>
2015-07-18 16:29:34 +0800

    Release v1.10.6 ⚡👊⚡

--- dist/jquery-ui-slider-pips.js
+++ dist/jquery-ui-slider-pips.js
@@ -1,4 +1,4 @@
-/*! jQuery-ui-Slider-Pips - v1.10.5 - 2015-07-05
+/*! jQuery-ui-Slider-Pips - v1.10.6 - 2015-07-18
 * Copyright (c) 2015 Simon Goellner <simey.me@gmail.com>; Licensed MIT */
 
 // PIPS
@@ -12,8 +12,7 @@
         pips: function( settings ) {
 
             var slider = this, i, j, p, collection = "",
-                mousedownHandlers = $._data( slider.element.get(0), "events").mousedown,
-                originalMousedown,
+                mousedownHandlers,
                 min = slider._valueMin(),
                 max = slider._valueMax(),
                 value = slider._value(),
@@ -53,12 +52,22 @@
 
             };
 
-            $.extend( options, settings );
+            if ( $.type( settings ) === "object" || $.type( settings ) === "undefined"  ) {
+                $.extend( options, settings );
+            } else {
+                if ( settings === "destroy" ) {
+                    destroy();
+                }
+                return;
+            }
 
-            slider.options.pipStep = options.step;
+
+            // we don't want the step ever to be a floating point.
+            slider.options.pipStep = Math.round( options.step );
 
             // get rid of all pips that might already exist.
             slider.element
+                .off( ".selectPip" )
                 .addClass("ui-slider-pips")
                 .find(".ui-slider-pip")
                 .remove();
@@ -188,6 +197,17 @@
                 }
 
                 return closestHandle;
+
+            }
+
+            function destroy() {
+
+                slider.element
+                    .off(".selectPip")
+                    .on("mousedown.slider", slider.element.data("mousedown-original") )
+                    .removeClass("ui-slider-pips")
+                    .find(".ui-slider-pip")
+                    .remove();
 
             }
 
@@ -377,9 +397,6 @@
 
             }
 
-            // we don't want the step ever to be a floating point.
-            slider.options.pipStep = Math.round( slider.options.pipStep );
-
             // create our first pip
             collection += createPip("first");
 
@@ -401,13 +418,28 @@
 
 
 
+            // store the mousedown handlers for later, just in case we reset
+            // the slider, the handler would be lost!
+            
+            if ( $._data( slider.element.get(0), "events").mousedown && 
+                $._data( slider.element.get(0), "events").mousedown.length ) {
+
+                mousedownHandlers = $._data( slider.element.get(0), "events").mousedown;
+
+            } else {
+
+                mousedownHandlers = slider.element.data("mousedown-handlers");
+
+            }
+
+            slider.element.data("mousedown-handlers", mousedownHandlers.slice() );
 
             // loop through all the mousedown handlers on the slider,
             // and store the original namespaced (.slider) event handler so
             // we can trigger it later.
             for( j = 0; j < mousedownHandlers.length; j++ ) {
                 if( mousedownHandlers[j].namespace === "slider" ) {
-                    originalMousedown = mousedownHandlers[j].handler;
+                    slider.element.data("mousedown-original", mousedownHandlers[j].handler );
                 }
             }
 
@@ -440,6 +472,7 @@
 
                     } else {
 
+                        var originalMousedown = slider.element.data("mousedown-original");
                         originalMousedown(e);
 
                     }
@@ -527,7 +560,17 @@
 
             };
 
-            $.extend( options, settings );
+            if ( $.type( settings ) === "object" || $.type( settings ) === "undefined"  ) {
+                $.extend( options, settings );
+            } else {
+                if ( settings === "destroy" ) {
+                    destroy();
+                }
+                return;
+            }
+
+
+
 
             if ( value < min ) { 
                 value = min; 
@@ -558,6 +601,19 @@
                 .addClass("ui-slider-float")
                 .find(".ui-slider-tip, .ui-slider-tip-label")
                 .remove();
+
+
+
+            function destroy() {
+
+                slider.element
+                    .off(".sliderFloat")
+                    .removeClass("ui-slider-float")
+                    .find(".ui-slider-tip, .ui-slider-tip-label")
+                    .remove();
+
+            }
+
 
             function getPipLabels( values ) {
 
@@ -676,16 +732,18 @@
             }
 
             // when slider changes, update handle tip label.
-            slider.element.on( options.event , function( e, ui ) {
+            slider.element
+                .off(".sliderFloat")
+                .on( options.event + ".sliderFloat", function( e, ui ) {
 
-                var uiValue = ( $.type( ui.value ) === "array" ) ? ui.value : [ ui.value ],
-                    val = options.formatLabel( getPipLabels( uiValue )[0] );
+                    var uiValue = ( $.type( ui.value ) === "array" ) ? ui.value : [ ui.value ],
+                        val = options.formatLabel( getPipLabels( uiValue )[0] );
 
-                $(ui.handle)
-                    .find(".ui-slider-tip")
-                    .html( val );
+                    $(ui.handle)
+                        .find(".ui-slider-tip")
+                        .html( val );
 
-            });
+                });
 
         }
 
