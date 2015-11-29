d9088f5ef5e676f614daa4a2c6cf216cd7112abf
simeydotme <simey.me@gmail.com>
2015-11-30 03:05:41 +0800

    Hack in to the core jQuery UI events for better event response - fixes #69
    not ideal, as if JQUI changes it's internal methods, will need to fix, but
    it does a good job of satisfying the original complaint.

--- src/js/jquery-ui-slider-pips.js
+++ src/js/jquery-ui-slider-pips.js
@@ -250,7 +250,7 @@
             // without this method the label is just treated like a part
             // of the slider and there's no accuracy in the selected value
 
-            function labelClick( label ) {
+            function labelClick( label, e ) {
 
                 if (slider.option("disabled")) {
                     return;
@@ -261,15 +261,16 @@
 
                 if ( slider.values() && slider.values().length ) {
 
-                    slider.element.slider("values", indexToChange, val );
+                    slider.options.values[ indexToChange ] = slider._trimAlignValue( val );
 
                 } else {
 
-                    slider.element.slider("value", val );
+                    slider.options.value = slider._trimAlignValue( val );
 
                 }
 
-                slider._lastChangedValue = indexToChange;
+                slider._refreshValue();
+                slider._change( e, indexToChange );
 
             }
 
@@ -501,7 +502,7 @@
 
                     if ( $target.is(".ui-slider-label") ) {
 
-                        labelClick( $target );
+                        labelClick( $target, e );
 
                         slider.element
                             .one("mouseup.selectPip", function() {
